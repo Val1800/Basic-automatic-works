9ce848d323082985118784f8a4c9924f91f5bfaf
ValeLopez18 <68173940+Val1800@users.noreply.github.com>
2021-03-07 11:24:55 -0500

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,9 +1,8 @@
 
+#Spam
 import pyautogui as p
 import time
 
-"""
-#Spam
 msg = input("¿Qué Mensaje desea enviar?\n")
 
 n = int(input("¿Cuántas copias del mensaje quiere enviar?\n"))
@@ -11,9 +10,12 @@ time.sleep(5.0)
 for x in range(n):
     p.write(msg)
     p.press("enter")
-"""
-"""
+
+
 #Square
+import pyautogui as p
+import time
+
 time.sleep(7.0)
 distance = 200
 while distance > 0:
@@ -22,18 +24,23 @@ while distance > 0:
     p.drag(0, distance, duration=0.5)   # move down
     p.drag(-distance, 0, duration=0.5)  # move left
     distance -= 5
-    p.drag(0, -distance, duration=0.5)  # move up
- """   
+    p.drag(0, -distance, duration=0.5)  # move up 
 
 
 #Upage
+import pyautogui as p
+import time
+
 time.sleep(10.0)
 p.hotkey("ctrl","n")
 p.write("http://www.konradlorenz.edu.co/")
 p.press("enter")
 
-"""
+
 #Playvideo
+import pyautogui as p
+import time
+
 time.sleep(7.0)
 p.hotkey("ctrl","t")
 p.write("https://www.youtube.com/watch?v=AsrzqSJd8jo&ab_channel=PeriodistaGen%C3%A9rico")
@@ -43,9 +50,12 @@ time.sleep(7.0)
 for x in range(3):
     p.press("right")
     time.sleep(4.0)
-"""
-"""
+
+
 #Circle
+import pyautogui as p
+import time
+
 time.sleep(5.0)
 import math
 
@@ -64,17 +74,4 @@ while R > 0:
         if i%6==0:
             p.dragTo(X+R*math.cos(math.radians(i)),Y+R*math.sin(math.radians(i)))
     R -= 10
-    """
-"""
-(X,Y) = p.position(x/4 + x/2,y/2)
-# offsetting by radius 
-p.drag(button='right')
-p.moveTo(X+R,Y)
-R = 300
-while R > 0:
-    for i in range(360):
-        # setting pace with a modulus 
-        if i%6==0:
-            p.dragTo(X+R*math.cos(math.radians(i)),Y+R*math.sin(math.radians(i)))
-    R -= 10
-"""
+    
